d15689cb97bf97b8344574991e398b07527b7848
Muhammad Surya Pratama <33799644+moryata@users.noreply.github.com>
2022-06-05 20:48:25 +0700

    Reduce Delay Timer

--- Allfaucet.js
+++ Allfaucet.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Allfaucet
 // @namespace    AutoClaim
-// @version      1.3
+// @version      1.4
 // @description  Auto Claim from manual faucet
 // @author       Moryata
 // @match        https://allfaucet.xyz/*
@@ -16,7 +16,7 @@
         if(unsafeWindow.hcaptcha && unsafeWindow.hcaptcha.getResponse() && unsafeWindow.hcaptcha.getResponse().length) {
             document.querySelector(".claim-button").click();
         }
-            }, 1025);
+            }, 525);
 
 var site = "https://allfaucet.xyz/faucet";
 
@@ -24,6 +24,6 @@ var site = "https://allfaucet.xyz/faucet";
         if($("*:contains('An Error Was Encountered')").length > 0) {
             window.location.href = site;
         }
-            }, 1050);
+            }, 250);
 
 })();

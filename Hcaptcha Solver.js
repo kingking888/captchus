f83258625b6813f4cc1a635220e0c1fd243903c4
Muhammad Surya Pratama <33799644+moryata@users.noreply.github.com>
2022-06-05 22:24:34 +0700

    Update Hcaptcha Solver.js

--- Hcaptcha Solver.js
+++ Hcaptcha Solver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hcaptcha Solver Mode OCR
 // @namespace     Captchus Model H
-// @version       3.6
+// @version       3.7
 // @description  Automatically solves Hcaptcha in browser
 // @author       Moryata
 // @match        https://*.hcaptcha.com/*hcaptcha-challenge*
@@ -748,7 +748,7 @@
     function preProcessImageMethod3(base64Image, imageUrl) {
         //Multi Contrast only brighten
         Jimp.read(base64Image).then(function(data) {
-            data.contrast(3).color([{
+            data.contrast().color([{
                 apply: 'brighten',
                 params: [20]
             }

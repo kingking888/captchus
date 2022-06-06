d333b1a457f04acea281a51f7e7c5e1537961764
Muhammad Surya Pratama <33799644+moryata@users.noreply.github.com>
2022-06-06 21:09:51 +0700

    Update Allfaucet.js

--- Allfaucet.js
+++ Allfaucet.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Allfaucet
 // @namespace    AutoClaim
-// @version      1.4
+// @version      1.5
 // @description  Auto Claim from manual faucet
 // @author       Moryata
 // @match        https://allfaucet.xyz/*
@@ -9,21 +9,24 @@
 // @grant        GM_info
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // ==/UserScript==
-
 (function() {
     'use strict';
     setInterval(function() {
-        if(unsafeWindow.hcaptcha && unsafeWindow.hcaptcha.getResponse() && unsafeWindow.hcaptcha.getResponse().length) {
+        if (unsafeWindow.hcaptcha && unsafeWindow.hcaptcha.getResponse() && unsafeWindow.hcaptcha.getResponse().length) {
             document.querySelector(".claim-button").click();
         }
-            }, 3525);
+    }, 2525);
 
-var site = "https://allfaucet.xyz/faucet";
+    var site = "https://allfaucet.xyz/faucet";
 
     setTimeout(function() {
-        if($("*:contains('An Error Was Encountered')").length > 0) {
+        if ($("*:contains('An Error Was Encountered')").length > 0) {
             window.location.href = site;
         }
-            }, 250);
+    }, 250);
+
+    setTimeout(function() {
+        window.location.assign(site);
+    }, 35000);
 
 })();

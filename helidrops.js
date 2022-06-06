08aa9bf8281a7d556debdec5926540a2910cecef
Muhammad Surya Pratama <33799644+moryata@users.noreply.github.com>
2022-06-06 21:50:12 +0700

    Refresh Force Page

--- helidrops.js
+++ helidrops.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Helidrops Free coins Auto Claim
 // @namespace    Auto Claim Helidrop
-// @version      1.1
+// @version      1.2
 // @description  Auto Claim from manual faucet
 // @author       Moryata
-// @match        https://helidrops.io/coins.php
+// @match        https://helidrops.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://helidrops.io
 // @grant        GM_info
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // ==/UserScript==
 (function() {
     'use strict';
-    let site = "https://helidrops.io/coins.php";
+    var site = "https://helidrops.io/coins.php";
 
     // solve the captcha and press the claim button
     setInterval(function() {
@@ -23,6 +23,6 @@
     // refresh page after 60 secondes
     setTimeout(function() {
         window.location.assign(site);
-    }, 30000);
+    }, 25000);
 
 })();
